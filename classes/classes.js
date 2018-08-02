3ee5417fe378765488c0fbd0905a9b3331ce834d
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 18:16:05 +0300

    woho

--- classes/classes.js
+++ classes/classes.js
@@ -1,1 +1,15 @@
 // Write all your JavaScript and JQuery code in this file! :)
+$("#bigButton").on("click", function(){
+	$("#message").addClass('big')
+
+})
+
+$("#hideButton").on("click", function(){
+	$("#message").addClass('hidden')
+	
+})
+
+$("#rotateButton").on("click", function(){
+	$("#message").addClass('rotated')
+	
+})
--- content/content.js
+++ content/content.js
@@ -1,1 +1,13 @@
 // Write all your JavaScript and JQuery code in this file! :)
+var num=0
+
+$('#counter').on("click", function() {
+	num ++
+	$("h1").eq(1).html(num);
+});
+
+$('#reset').on("click", function() {
+	num = 0 
+	$("h1").eq(1).html(num);
+});
+
